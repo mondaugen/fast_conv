0b2fe3a80773e440968b2f4581b4850fa4f25b9d
mondaugen <nicholas.esterer@gmail.com>
2017-04-22 14:00:32 -0400

    Added profiling script

--- run_pc_test_cg.sh
+++ run_pc_test_cg.sh
@@ -2,5 +2,6 @@
 seq 1 10 | awk '{ print 200 " " 4096 " " 2**$0 }' | \
     valgrind --tool=callgrind --instr-atstart=no \
     ./part_conv.bin
-find . -name "callgrind.out*" | awk 'BEGIN {FS="."} { print $5 }'
+#find . -name "callgrind.out*" | awk 'BEGIN {FS="."} { print $5 }'
+find -E . -regex "\./callgrind\.out\.[0-9]+\.[0-9]+" | awk 'BEGIN {FS="."} { print "echo \"" $5 " " "$( callgrind_annotate " $0 " | grep TOTALS | tr -d \",\" )\"" }' | bash | awk '{ print $1 " " $2 }'
 
